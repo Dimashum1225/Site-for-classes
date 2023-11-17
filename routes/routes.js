6619ce5f7124ce350a8809daaca7a384d44a7291
Dima Shumatbaev <sumatbaev7@gmail.com>
2023-11-18 00:50:34 +0600

    чучуть того сего

--- routes/routes.js
+++ routes/routes.js
@@ -6,11 +6,12 @@ const indexRoutes = require('./index.Routes');
 const loginRoutes = require('./loginRoutes');
 const logoutRoutes = require('./logoutRoutes');
 const registerRoutes = require('./registerRoutes');
+const addNewsRoutes = require('./addNewsRoutes')
 
 router.use('/',indexRoutes);
 router.use('/dashboard',dashboardRoutes);
 router.use('/login',loginRoutes);
 router.use('/logout',logoutRoutes);
 router.use('/register',registerRoutes);
-
+router.use('/dashboard',addNewsRoutes)
 module.exports=router;
--- server.js
+++ server.js
@@ -3,28 +3,40 @@ const mongoose = require('mongoose');
 const app = express();
 const connectToMongoDB = require('./database/connection');
 const path = require('path');
-const routes=require('./routes/routes');
-const session = require('express-session')
+const routes = require('./routes/routes');
+const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
 
 require('dotenv').config();
+
 const store = new MongoDBStore({
-    uri:process.env.MONGODB_URI,
-    collection:'sessions'
-})
-app.set('views',path.join(__dirname, 'views'));
-app.set('view engine','ejs');
+    uri: process.env.MONGODB_URI,
+    collection: 'sessions'
+});
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(session({
-    secret:'secret key',
-    resave:false,
-    saveUninitialized:false,
-    store:store
-}))
-app.use('/',routes);
+    secret: 'secret key',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+}));
+
+app.use('/', routes);
+app.use('/css', express.static(path.join(__dirname, 'public', 'css')));
+app.use('/img/avatars', express.static(path.join(__dirname, 'public', 'image', 'avatars')));
+app.use('/uploads', express.static(path.join(__dirname, 'public', 'uploads')));
+
+// Добавление папки 'views' как статической директории
+app.use('/people', express.static(path.join(__dirname, 'people')));
+
+
 app.get('/', (req, res) => {
-    res.render('index',{session:req.session});
+    res.render('index', { session: req.session });
 });
+
 app.get('/register', (req, res) => {
     res.render('register');
 });
@@ -32,16 +44,19 @@ app.get('/register', (req, res) => {
 app.get('/login', (req, res) => {
     res.render('login');
 });
+
 async function start() {
     try {
         const uri = await connectToMongoDB();
         await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-        app.listen(3000, () => {
-            console.log('Сервер запущен на порту 3000');
-        });
 
+        const PORT = process.env.PORT || 3000;
+        app.listen(PORT, () => {
+            console.log(`Сервер запущен на порту ${PORT}`);
+        });
     } catch (error) {
-        console.log(error);
+        console.error('Ошибка при подключении к базе данных:', error);
+        // Добавить обработку ошибки для пользователя, если необходимо
     }
 }
 
